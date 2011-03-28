d3771e6c1df7024f50526e2c6c0edbc7f6443de3
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2011-03-28 22:50:44 +0400

    Fix paths in the build script.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp nacl.cache nacl.cache.tmp
-PATH=$HOME/root/bin:$PATH CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib PKG_CONFIG_LIBDIR=$HOME/root/nacl64/lib/pkgconfig \
-  ./configure --host=nacl64 --prefix=$HOME/root/nacl64 --cache-file=nacl.cache.tmp
-PATH=$HOME/root/bin:$PATH make
-PATH=$HOME/root/bin:$PATH make install
+PATH=$HOME/root/nacl-sdk/bin:$PATH CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib PKG_CONFIG_LIBDIR=$HOME/root/nacl-sdk/nacl64/usr/lib/pkgconfig \
+  ./configure --host=nacl64 --prefix=$HOME/root/nacl-sdk/nacl64/usr --cache-file=nacl.cache.tmp
+PATH=$HOME/root/nacl-sdk/bin:$PATH make
+PATH=$HOME/root/nacl-sdk/bin:$PATH make install
 
