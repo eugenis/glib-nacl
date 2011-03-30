d877d82d99995cd21e8a799ff27e793961e17aac
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2011-03-30 23:09:42 +0400

    Improve build.sh.

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+ROOT=${NACL_TOOLCHAIN_ROOT:-$HOME/root/nacl-sdk}
+
 cp nacl.cache nacl.cache.tmp
-PATH=$HOME/root/nacl-sdk/bin:$PATH CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib PKG_CONFIG_LIBDIR=$HOME/root/nacl-sdk/nacl64/usr/lib/pkgconfig \
-  ./configure --host=nacl64 --prefix=$HOME/root/nacl-sdk/nacl64/usr --cache-file=nacl.cache.tmp
-PATH=$HOME/root/nacl-sdk/bin:$PATH make
-PATH=$HOME/root/nacl-sdk/bin:$PATH make install
+PATH=$ROOT/bin:$PATH CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib PKG_CONFIG_LIBDIR=$ROOT/nacl64/usr/lib/pkgconfig \
+  ./configure --host=nacl64 --prefix=$ROOT/nacl64/usr --cache-file=nacl.cache.tmp
+PATH=$ROOT/bin:$PATH make
+PATH=$ROOT/bin:$PATH make install
 
